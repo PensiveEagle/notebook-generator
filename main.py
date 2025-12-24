ba9ac1bd2837cebbd09bb780d03ac2d9cedd1954
PensiveEagle <halljf96@gmail.com>
2025-12-24 20:04:06 +0000

    Created pdf pages based on the topics csv

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
+# --------------- Import packages --------------- #
 from fpdf import FPDF
+import pandas as pd
 
+# --------------- Set defaults --------------- #
+FILENAME = "topics.csv"
+
+# --------------- Impor data --------------- #
+df = pd.read_csv( "topics.csv" )
+
+# --------------- Create pdf object --------------- #
 pdf = FPDF( orientation = "P", unit = "mm", format = "A4" )
+pdf.set_font( family = "Times", style = "B", size = 24 )
+pdf.set_text_color( 100, 100, 100)
 
-pdf.add_page()
 
-pdf.set_font( family = "Times", style = "B", size = 12 )
+# --------------- Add pages --------------- #
+for index, row in df.iterrows():
+    
+    pdf.add_page()
+    
+    pdf.cell( w = 0, h = 12, 
+             txt = row["Topic"], align = "L", 
+             ln = 1)
+    
+    pdf.cell( w = 0, h = 0,
+             ln = 1, border = 1 )
+    
+    for x in range( 1, row["Pages"] ):
+        pdf.add_page()
 
-pdf.cell( w = 0, h = 12, txt = "Hello World", align = "L", border = 1, ln = 1)
-pdf.cell( w = 0, h = 12, txt = "Hello You", align = "L", border = 1, ln = 1)
 
 pdf.output( "output.pdf" )
+
+
+
+'''
+______
+   |  |
+   |__|
+   |  |
+\_/|  |
+
+'''
